cb4725c77041e5c747bd251ec54b254ffecd3168
angrypin <arifinmblygf@gmail.com>
2022-03-05 23:47:40 +0700

    remove unused

--- assets/js/customize.js
+++ assets/js/customize.js
@@ -1,18 +1,7 @@
 localStorage.theme = localStorage.theme !== undefined ? localStorage.theme : 'light';
 dark = localStorage.theme === 'dark';
 
-const colors = {
-	accent: {
-		dark: '#588da7',
-		light: '#faca0d'
-	}, bghover: {
-		dark: 'rgba(81, 142, 161, .6)',
-		light: 'rgba(250, 202, 13, .6)'
-	}, bgcolor: {
-		dark: '#1b1c1d',
-		light: '#fff'
-	}
-}, toggleTheme = (theme) => {
+const toggleTheme = (theme) => {
 	localStorage.theme = theme;
 	dark = localStorage.theme === 'dark';
 
@@ -20,12 +9,6 @@ const colors = {
 	dark
 		? $('head').append('<link rel="stylesheet" type="text/css" href="assets/css/dark.css">')
 		: $('link[href*="dark.css"]').remove();
-	/* $('.head').css('background-image', `url(assets/images/${theme}.jpg)`);
-	
-	$(':root')
-		.css('--accent', colors.accent[theme])
-		.css('--bghover', colors.bghover[theme])
-		.css('--bgcolor', colors.bgcolor[theme]); */
 };
 
 $('.theme-changer').click(() => {
