6ec247e5acb6898a3266d580cec63a29574fe976
angrypin <arifinmblygf@gmail.com>
2022-03-05 21:51:26 +0700

    dark theme

---
+++ assets/js/customize.js
@@ -0,0 +1,32 @@
+localStorage.theme = localStorage.theme !== undefined ? localStorage.theme : 'light';
+dark = localStorage.theme === 'dark';
+
+const colors = {
+	accent: {
+		dark: '#588da7',
+		light: '#faca0d'
+	}, bghover: {
+		dark: 'rgba(81, 142, 161, .6)',
+		light: 'rgba(250, 202, 13, .6)'
+	}, bgcolor: {
+		dark: '#1b1c1d',
+		light: '#fff'
+	}
+}, toggleTheme = (theme) => {
+	localStorage.theme = theme;
+	dark = localStorage.theme === 'dark';
+
+	$('.theme-changer').html(`<i class="${dark ? 'moon' : 'sun'} icon"></i>${theme} theme`);
+	$('.head').css('background-image', `url(assets/images/${theme}.jpg)`);
+	
+	$(':root')
+		.css('--accent', colors.accent[theme])
+		.css('--bghover', colors.bghover[theme])
+		.css('--bgcolor', colors.bgcolor[theme]);
+};
+
+$('.theme-changer').click(() => {
+	toggleTheme(dark ? 'light' : 'dark');
+});
+
+toggleTheme(localStorage.theme);
